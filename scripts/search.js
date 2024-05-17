3adcea08a9d62483a61c06052fa32725cef84fc0
Wordhater <95009727+wordhater@users.noreply.github.com>
2024-05-17 13:18:32 +1000

    Inital commit

---
+++ scripts/search.js
@@ -0,0 +1,70 @@
+// Gloabl Variables
+let storage = []
+let meals = []
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+// general functions
+
+const getJSON = async url => {
+    const response = await fetch(url);
+    if (!response.ok)
+        throw new Error(response.statusText);
+    const data = response.json();
+    return data;
+}
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+function formattoHTML(text){
+    let out = text.replace(/\n/gi, "<br>")
+    return out
+}
+// search
+function getmeal(element) {
+    console.log('searching for who asked')
+    let query = document.getElementById(element).value
+    console.log(query)
+    for (let i = 0; i < document.getElementsByClassName('search-result').length; i++){
+        document.getElementsByClassName('search-result')[i].remove()
+    }
+    document.getElementById('search-progress-indicator').innerHTML = "searching"
+    getJSON(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`).then(data => {
+        console.log(data)
+        storage[0] = data
+        let items = data.meals.length
+        for (let i = 0; i < items; i++) {
+            let html = create(
+`<div class="search-result" id="result-${i}" onclick="resultselected('${i}')">
+    <img src="${data.meals[i].strMealThumb}" alt="Image of ${data.meals[i].strMeal}">
+    <p class="result-text">${data.meals[i].strMeal}</p>
+</div>`)
+            document.getElementById('results').appendChild(html)
+        }
+        
+    }).catch(error => {
+        console.error(error);
+    });
+    document.getElementById('search-progress-indicator').innerHTML = ""
+}
+
+function resultselected(result){
+    console.log(result)
+    let meal = storage[0].meals[result]
+    console.log(meal)
+    meals[result] = meal
+    let current = document.getElementsByClassName('meal-list-item').length
+    let html = create(`
+<div class="meal-list-item" id="${current}">
+    <h2 class="meal-date"><b>${days[current]}</b></h2>
+    <img src="${meal.strMealThumb}" alt="meal image" class="meal-list-image">
+    <p class="meal-list-text">${(meal.strMeal).replace(" ", "<br>")}</p>
+</div>`)
+    document.getElementById('meal-list').appendChild(html)
+    document.querySelectorAll('.search-result').forEach(e => e.remove());
+    document.getElementById('close').click()
+}
